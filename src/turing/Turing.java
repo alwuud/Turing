1034b5fd6f9e41e38bcf11255c7283deec8c8a3e
alwuud <dahh.drag@gmail.com>
2016-10-29 08:00:07 -0600

    projecto Turing

---
+++ src/turing/Turing.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turing;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alwud
+ */
+public class Turing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        FilesIO aux= new FilesIO("/home/alwud/Desktop/MATRIZ.txt");
+        aux.read();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+}
